6237a50fd7dbe8b3436349b930682a48cc831437
Invitado <invitado@MacBook-Air-de-Ricardo.local>
2025-02-06 00:23:50 -0600

    tipos de variables

---
+++ tipos-de-dato.js
@@ -0,0 +1,24 @@
+// este es un comentario en java script//
+
+// console.log es una funcion que se utiliza para imprimir en consola
+console.log("hola mundo");
+
+    // declarando variables y asignacion de valores
+    var nombre = "Carlos"; //string
+    var pais = 20;  //number
+    var sueldo = 1.900; // number
+    var casada = false; // boolean
+    var vacunas = true; // boolean
+    var hijo = null; // null 
+    var dato;
+
+    //typeof para saber el tipo de dato de una variable
+    console.log(typeof(nombre));
+    console.log(typeof(pais));
+    console.log(typeof(sueldo));
+    console.log(typeof(casada));
+    console.log(typeof(vacunas));
+    console.log(typeof(hijos));
+    console.log(typeof(dato));
+
+    console.log(typeof 42);
